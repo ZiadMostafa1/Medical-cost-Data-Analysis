3e01a996cc0ddef3cde9a1b400db0ac3a1a6d4a7
ZiadMostafa1 <ziad.zero1245@gmail.com>
2024-03-17 12:48:10 +0200

    edited the app

--- app.py
+++ app.py
@@ -16,26 +16,26 @@ st.title('Medical Cost Data Analysis')
 st.subheader('Histogram of Age')
 fig_hist_age = go.Figure(data=[go.Histogram(x=df['age'], nbinsx=20, marker_color='skyblue')])
 fig_hist_age.update_layout(bargap=0.1)  # Adjust this value to change the gap size
-st.plotly_chart(fig_hist_age)
+st.plotly_chart(fig_hist_age, use_container_width=True)
 
 # Countplot of Sex
 st.subheader('Countplot of Sex')
 fig_count_sex = px.bar(df['sex'].value_counts().reset_index(), x='index', y='sex', color='index', color_discrete_sequence=['skyblue'])
-st.plotly_chart(fig_count_sex)
+st.plotly_chart(fig_count_sex,use_container_width=True)
 
 # Charges by Age (Line Plot)
 st.subheader('Charges by Age (Line Plot)')
 df_avg_charges = df.groupby('age')['charges'].mean().reset_index()
 fig_line_charges_age = px.line(df_avg_charges, x='age', y='charges', color_discrete_sequence=['skyblue'])
-st.plotly_chart(fig_line_charges_age)
+st.plotly_chart(fig_line_charges_age, use_container_width=True)
 
 # Charges by Age (Line Plot - different color)
 st.subheader('Charges by Age (Line Plot - different color)')
 df_avg_charges = df.groupby('age')['charges'].mean().reset_index()
 fig_line_charges_age_skyblue = px.line(df_avg_charges, x='age', y='charges', color_discrete_sequence=['orange'])
-st.plotly_chart(fig_line_charges_age_skyblue)
+st.plotly_chart(fig_line_charges_age_skyblue, use_container_width=True)
 
 # Barplot of Region
 st.subheader('Barplot of Region')
 fig_bar_region = px.bar(df['region'].value_counts().reset_index(), x='index', y='region', color='index', color_discrete_sequence=['skyblue'])
-st.plotly_chart(fig_bar_region)
+st.plotly_chart(fig_bar_region, use_container_width=True)
